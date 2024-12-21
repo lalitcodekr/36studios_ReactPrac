cd4bfd3066ee9d3d7bdabf21205c3df1d65a1abd
Pyda Lalit Kumar <p.lalit_kr@Lalits-MacBook-Air.local>
2024-12-21 17:18:17 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,248 @@
+import "./index.css";
+import Canvas from "./Canvas";
+import data from "./data";
+import LocomotiveScroll from "locomotive-scroll";
+import { useEffect, useState, useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { Circ, Expo } from "gsap/all";
+
+function App() {
+  const [showCanvas, setShowCanvas] = useState(false);
+  const headingref = useRef(null);
+  const growingSpan = useRef(null);
+  const footerRef = useRef(null);
+
+  useEffect(() => {
+    const locomotiveScroll = new LocomotiveScroll();
+  }, []);
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      setShowCanvas((prevShowCanvas) => {
+        if (!prevShowCanvas) {
+          gsap.set(growingSpan.current, {
+            top: e.clientY,
+            left: e.clientX,
+          });
+
+          gsap.to("body", {
+            color: "#000",
+            backgroundColor: "#fd2c2a",
+            duration: 1.2,
+            ease: "power2.inOut",
+          });
+
+          gsap.to(footerRef.current, {
+            backgroundColor: "#fd2c2a",
+            duration: 1.2,
+            ease: "power2.inOut"
+          });
+
+          gsap.to(growingSpan.current, {
+            scale: 1000,
+            duration: 2,
+            ease: "power2.inOut",
+            onComplete: () => {
+              gsap.set(growingSpan.current, {
+                scale: 0,
+                clearProps: "all",
+              });
+            },
+          });
+        } else {
+          gsap.to("body", {
+            color: "#fff",
+            backgroundColor: "#000",
+            duration: 1.2,
+            ease: "power2.inOut",
+          });
+
+          gsap.to(footerRef.current, {
+            backgroundColor: "#000",
+            duration: 1.2,
+            ease: "power2.inOut"
+          });
+        }
+
+        return !prevShowCanvas;
+      });
+    };
+
+    const headingElement = headingref.current;
+    headingElement.addEventListener("click", handleClick);
+
+    // Clean up event listener on unmount
+    return () => headingElement.removeEventListener("click", handleClick);
+  }, []);
+
+  return (
+    <>
+      <span
+        ref={growingSpan}
+        className="growing rounded-full block fixed top-[-20px] left-[-20px] w-5 h-5"
+      ></span>
+      <div className="w-full relative min-h-screen font-['Helvetica_Now_Display']">
+        {showCanvas &&
+          data[0].map((canvasdets, index) => <Canvas details={canvasdets} />)}
+        <div className="w-full relative z-[1] h-screen ">
+          <nav className="w-full p-8 flex justify-between z-50">
+            <div className="brand text-2xl font-md">thirtysixstudios</div>
+            <div className="links flex gap-10">
+              {[ 
+                "What we do",
+                "Who we are",
+                "How we give back",
+                "Talk to us",
+              ].map((link, index) => (
+                <a
+                  key={index}
+                  href={`#${link.toLowerCase()}`}
+                  className="text-md hover:text-gray-300"
+                >
+                  {link}
+                </a>
+              ))}
+            </div>
+          </nav>
+          <div className="textcontainer  w-full px-[20%]">
+            <div className="text w-[50%]">
+              <h3 className="text-4xl leading-[1.2]">
+                At Thirtysixstudio, we build immersive digital experiences for
+                brands with a purpose.
+              </h3>
+              <p className="text-lg w-[80%] mt-10 font-normal">
+                We are a team of designers, developers, and strategists who are
+                passionate about creating digital experiences that are both
+                beautiful and functional.
+              </p>
+              <p className="text-md mt-10">scroll</p>
+            </div>
+          </div>
+          <div className="w-full absolute bottom-0 left-0">
+            <h1
+              ref={headingref}
+              className="text-[17rem] font-normal tracking-tight leading-none pl-5"
+            >
+              Thirtysixstudios
+            </h1>
+          </div>
+        </div>
+      </div>
+      <div className="w-full relative min-h-screen mt-32 px-10">
+        {showCanvas &&
+          data[3].map((canvasdets, index) => <Canvas details={canvasdets} />)}
+        <h1 className="text-8xl tracking-tighter">about the brand</h1>
+        <p className="text-4xl leading-[1.8] w-[80%] mt-10 font-light">
+          we are a team of designers, developers, and strategists who are
+          passionate about creating digital experiences that are both beautiful
+          and functional, we are a team of designers, developers, and
+          strategists who are passionate about creating digital experiences that
+          are both beautiful and functional.
+        </p>
+
+        <div className="carousel rounded-md py-36">
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+              alt="Burger" />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+              alt="Burger" />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+              alt="Burger" />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+              alt="Burger" />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+              alt="Burger" />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+              alt="Burger" />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+              alt="Burger" />
+          </div>
+        </div>
+      </div>
+      <div ref={footerRef} className="w-full h-[40vh] bg-black">
+          <footer className="footer bg-base-300 text-base-content p-10 h-[40vh] relative">
+            {showCanvas &&
+              data[2].map((canvasdets, index) => (
+                <Canvas key={index} details={canvasdets} />
+              ))}
+            <div className="footer-content relative z-10">
+              <nav>
+                <h6 className="footer-title">Services</h6>
+                <a className="link link-hover">Branding  </a>
+                <a className="link link-hover">Design  </a>
+                <a className="link link-hover">Marketing  </a>
+                <a className="link link-hover">Advertisement  </a>
+              </nav>
+              <nav>
+                <h6 className="footer-title">Company</h6>
+                <a className="link link-hover">About us  </a>
+                <a className="link link-hover">Contact  </a>
+                <a className="link link-hover">Jobs  </a>
+                <a className="link link-hover">Press kit  </a> 
+              </nav>
+              <nav>
+                <h6 className="footer-title">Social</h6>
+                <div className="grid grid-flow-col gap-4">
+                  <a className="hover:text-primary transition-colors">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      className="fill-current">
+                      <path
+                        d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                    </svg>
+                  </a>
+                  <a className="hover:text-primary transition-colors">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      className="fill-current">
+                      <path
+                        d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/>
+                    </svg>
+                  </a>
+                  <a className="hover:text-primary transition-colors">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      className="fill-current">
+                      <path
+                        d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zm0-2.163c-3.259 0-3.667.014-4.947.072-4.358.2-6.78 2.618-6.98 6.98-.059 1.281-.073 1.689-.073 4.948 0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98 1.281.058 1.689.072 4.948.072 3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98-1.281-.059-1.69-.073-4.949-.073zm0 5.838c-3.403 0-6.162 2.759-6.162 6.162s2.759 6.163 6.162 6.163 6.162-2.759 6.162-6.163c0-3.403-2.759-6.162-6.162-6.162zm0 10.162c-2.209 0-4-1.79-4-4 0-2.209 1.791-4 4-4s4 1.791 4 4c0 2.21-1.791 4-4 4zm6.406-11.845c-.796 0-1.441.645-1.441 1.44s.645 1.44 1.441 1.44c.795 0 1.439-.645 1.439-1.44s-.644-1.44-1.439-1.44z"/>
+                    </svg>
+                  </a>
+                </div>
+              </nav>
+            </div>
+          </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
